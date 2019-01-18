95a90321c83a7fc55c3c07b6830232c6ab28f9ef
Fabiano <fabianoyoschitaki@gmail.com>
2019-01-18 11:36:04 -0200

    created maven project

---
+++ src/main/java/br/com/javachallenge/processdata/App.java
@@ -0,0 +1,13 @@
+package br.com.javachallenge.processdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
